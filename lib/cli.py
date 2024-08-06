050beeb6b62663ce45801fad2b62e3d51e7bc1c7
Tess <tess.mueske@gmail.com>
2024-08-06 13:47:00 -0700

    cli updates

--- lib/cli.py
+++ lib/cli.py
@@ -1,20 +1,15 @@
 # lib/cli.py
 
 from helpers import (
-    exit_program,
-    list_destination_activities,
-    list_destinations,
-    list_destinations_by_name,
-    find_destination_by_name,
-    find_destination_by_id,
+    exit,
     create_destination,
+    list_all_destinations_by_name,
+    find_destination_by_name,
     update_destination,
     delete_destination,
-    list_activities,
-    list_activities_by_name,
-    find_activities_by_name,
-    find_activity_by_id,
     create_activity,
+    list_all_activities_by_name,
+    find_activity_by_name,
     update_activity,
     delete_activity
 )
@@ -22,70 +17,45 @@ from helpers import (
 def main():
     while True:
         menu()
-        choice = input("> ")
+        choice = input("... ")
         if choice == "0":
-            exit_program()
+            exit()
         elif choice == "1":
-            helper_1()
+            create_destination()
+        elif choice == "2":
+            list_all_destinations_by_name()
+        elif choice == "3":
+            find_destination_by_name()
+        elif choice == "4":
+            update_destination()
+        elif choice == "5":
+            delete_destination()
+        elif choice == "6":
+            create_activity()
+        elif choice == "7":
+            list_all_activities_by_name()
+        elif choice == "8":
+            find_activity_by_name()
+        elif choice == "9":
+            update_activity()
+        elif choice == "10":
+            delete_activity()
         else:
-            print("Invalid choice")
-
-# def main():
-#     while True:
-#         menu()
-#         choice = input("> ")
-#         if choice == "0":
-#             exit_program()
-#         elif choice == "1":
-#             list_departments()
-#         elif choice == "2":
-#             find_department_by_name()
-#         elif choice == "3":
-#             find_department_by_id()
-#         elif choice == "4":
-#             create_department()
-#         elif choice == "5":
-#             update_department()
-#         elif choice == "6":
-#             delete_department()
-#         elif choice == "7":
-#             list_employees()
-#         elif choice == "8":
-#             find_employee_by_name()
-#         elif choice == "9":
-#             find_employee_by_id()
-#         elif choice == "10":
-#             create_employee()
-#         elif choice == "11":
-#             update_employee()
-#         elif choice == "12":
-#             delete_employee()
-#         elif choice == "13":
-#             list_department_employees()
-#         else:
-#             print("Invalid choice")
+            print("Invalid choice :(")
 
 def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
-
-# def menu():
-#     print("Please select an option:")
-#     print("0. Exit the program")
-#     print("1. List all departments")
-#     print("2. Find department by name")
-#     print("3. Find department by id")
-#     print("4: Create department")
-#     print("5: Update department")
-#     print("6: Delete department")
-#     print("7. List all employees")
-#     print("8. Find employee by name")
-#     print("9. Find employee by id")
-#     print("10: Create employee")
-#     print("11: Update employee")
-#     print("12: Delete employee")
-#     print("13: List all employees in a department")
+    print("please select an option:")
+    print("0. exit")
+    print("1. create a destination")
+    print("2. list all destinations by name")
+    print("3. find a destination by its name")
+    print("4. update a destination")
+    print("5. delete a destination")
+    print("6. create an activity")
+    print("7. list all activities by name")
+    print("8. find an activity by its name")
+    print("9. update an activity")
+    print("10. delete an activity")
 
 if __name__ == "__main__":
     main()
