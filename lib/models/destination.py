20507992e2197ea3a449b38d559f70f48c1b88eb
Tess <tess.mueske@gmail.com>
2024-08-07 17:21:12 -0700

    complete destination class methods

--- lib/models/destination.py
+++ lib/models/destination.py
@@ -71,90 +71,59 @@ class Destination:
         CURSOR.execute(sql, (self.name))
         CONN.commit()
 
-    # def delete(self):
-    #     """Delete the table row corresponding to the current Department instance,
-    #     delete the dictionary entry, and reassign id attribute"""
+    def delete(self):
+        """Delete the table row corresponding to the current Destination instance,
+        delete the dictionary entry, and reassign id attribute"""
 
-    #     sql = """
-    #         DELETE FROM departments
-    #         WHERE id = ?
-    #     """
+        sql = """
+            DELETE FROM destination
+            WHERE id = ?
+        """
 
-    #     CURSOR.execute(sql, (self.id,))
-    #     CONN.commit()
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
 
-     ##### Delete the dictionary entry using id as the key
-      ###  #del type(self).all[self.id]
+        del type(self).all[self.id]
 
-      ###  # Set the id to None
-     ##   #self.id = None
+        self.id = None
 
-    # @classmethod
-    # def instance_from_db(cls, row):
-    #     """Return a Department object having the attribute values from the table row."""
+    @classmethod
+    def get_all(cls):
+        """Return a list containing a Destination object per row in the table"""
+        sql = """
+            SELECT *
+            FROM destination
+        """
 
-    #     # Check the dictionary for an existing instance using the row's primary key
-    #     department = cls.all.get(row[0])
-    #     if department:
-    #         # ensure attributes match row values in case local instance was modified
-    #         department.name = row[1]
-    #         department.location = row[2]
-    #     else:
-    #         # not in dictionary, create new instance and add to dictionary
-    #         department = cls(row[1], row[2])
-    #         department.id = row[0]
-    #         cls.all[department.id] = department
-    #     return department
+        rows = CURSOR.execute(sql).fetchall()
 
-    # @classmethod
-    # def get_all(cls):
-    #     """Return a list containing a Department object per row in the table"""
-    #     sql = """
-    #         SELECT *
-    #         FROM departments
-    #     """
+        return [cls.instance_from_db(row) for row in rows]
 
-    #     rows = CURSOR.execute(sql).fetchall()
+    @classmethod
+    def find_by_name(cls, name):
+        """Return a Destination object corresponding to first table row matching specified name"""
+        sql = """
+            SELECT *
+            FROM destination
+            WHERE name is ?
+        """
 
-    #     return [cls.instance_from_db(row) for row in rows]
+        row = CURSOR.execute(sql, (name,)).fetchone()
+        return cls.instance_from_db(row) if row else None
 
-    # @classmethod
-    # def find_by_id(cls, id):
-    #     """Return a Department object corresponding to the table row matching the specified primary key"""
-    #     sql = """
-    #         SELECT *
-    #         FROM departments
-    #         WHERE id = ?
-    #     """
+    def activities(self):
+        """Return list of activities associated with current destination"""
+        from models.activity import Activity
+        sql = """
+            SELECT * FROM activity
+            WHERE destination_id = ?
+        """
+        CURSOR.execute(sql, (self.id,),)
 
-    #     row = CURSOR.execute(sql, (id,)).fetchone()
-    #     return cls.instance_from_db(row) if row else None
-
-    # @classmethod
-    # def find_by_name(cls, name):
-    #     """Return a Department object corresponding to first table row matching specified name"""
-    #     sql = """
-    #         SELECT *
-    #         FROM departments
-    #         WHERE name is ?
-    #     """
-
-    #     row = CURSOR.execute(sql, (name,)).fetchone()
-    #     return cls.instance_from_db(row) if row else None
-
-    # def employees(self):
-    #     """Return list of employees associated with current department"""
-    #     from models.employee import Employee
-    #     sql = """
-    #         SELECT * FROM employees
-    #         WHERE department_id = ?
-    #     """
-    #     CURSOR.execute(sql, (self.id,),)
-
-    #     rows = CURSOR.fetchall()
-    #     return [
-    #         Employee.instance_from_db(row) for row in rows
-    #     ]
+        rows = CURSOR.fetchall()
+        return [
+            Activity.instance_from_db(row) for row in rows
+        ]
 
 
 # Destinations:
