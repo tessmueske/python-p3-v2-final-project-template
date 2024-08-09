42b255101689951b8bc8ef720e313a14882a1921
Tess <tess.mueske@gmail.com>
2024-08-09 13:26:46 -0600

    activity sql orm edits

--- lib/models/activity.py
+++ lib/models/activity.py
@@ -54,14 +54,16 @@ class Activity:
         return self._plan_ahead
 
     @property
-    def destination_name(self):
-        return self._destination_name
+    def destination_id(self):
+        return self._destination_id
 
-    @destination_name.setter
-    def destination_name(self, value):
-        if not isinstance(value, str):
-            raise Exception("destination name must be only letters of the alphabet")
-        return self._destination_name
+    @destination_id.setter
+    def destination_id(self, destination_id):
+        if type(destination_id) is int and Destination.find_by_id(destination_id):
+            self._destination_id = destination_id
+        else:
+            raise ValueError(
+                "depstination_id must reference a destination in the database")
 
     @classmethod
     def create_table(cls):
@@ -74,13 +76,13 @@ class Activity:
                 length_of_time INTEGER,
                 plan_ahead BOOLEAN,
                 destination_id INTEGER,
-                FOREIGN KEY (destination_id) REFERENCES destinations(id)
+                FOREIGN KEY (destination_id) REFERENCES destinations(id))
             )
         """
         CURSOR.execute(sql)
         CONN.commit()
 
-    @classmethod
+     @classmethod
     def drop_table(cls):
         """ Drop the table that persists Activity instances """
         sql = """
@@ -90,12 +92,12 @@ class Activity:
         CONN.commit()
 
     def save(self):
-        """ Insert a new row with the name, job title, and destination name values of the current Activity object.
+        """ Insert a new row with the name, price, length of time, plan ahead, and destination id values of the current Activity object.
         Update object id attribute using the primary key value of new row.
         Save the object in local dictionary using table row's PK as dictionary key"""
         sql = """
-                INSERT INTO activities (name, price, length_of_time, plan_ahead)
-                VALUES (?, ?, ?, ?)
+                INSERT INTO activities (name, price, length_of_time, plan_ahead, destination_id)
+                VALUES (?, ?, ?, ?, ?)
         """
 
         CURSOR.execute(sql, (self.name, self.price, self.length_of_time, self.plan_ahead, self.destination_id))
@@ -108,10 +110,11 @@ class Activity:
         """Update the table row corresponding to the current Activity instance."""
         sql = """
             UPDATE activities
-            SET name = ?, price = ?, length_of_time = ? plan_ahead = ?
+            SET name = ?, price = ?, length_of_time = ?, plan_ ahead = ?, destination_id = ?
             WHERE id = ?
         """
-        CURSOR.execute(sql, (self.name, self.price, self.length_of_time, self.plan_ahead, self.id))
+        CURSOR.execute(sql, (self.name, self.price, self.length_of_time, self.plan_ahead,
+                             self.destination_id, self.id))
         CONN.commit()
 
     def delete(self):
@@ -126,14 +129,16 @@ class Activity:
         CURSOR.execute(sql, (self.id,))
         CONN.commit()
 
+        # Delete the dictionary entry using id as the key
         del type(self).all[self.id]
 
+        # Set the id to None
         self.id = None
 
     @classmethod
-    def create(cls, name, price, length_of_time, plan_ahead):
+    def create(cls, name, price, length_of_time, plan_ahead, destination_id):
         """ Initialize a new Activity instance and save the object to the database """
-        activity = cls(name, price, length_of_time, plan_ahead)
+        activity = cls(name, price, length_of_time, plan_ahead, destination_id)
         activity.save()
         return activity
 
@@ -141,14 +146,18 @@ class Activity:
     def instance_from_db(cls, row):
         """Return an Activity object having the attribute values from the table row."""
 
+        # Check the dictionary for  existing instance using the row's primary key
         activity = cls.all.get(row[0])
         if activity:
+            # ensure attributes match row values in case local instance was modified
             activity.name = row[1]
             activity.price = row[2]
             activity.length_of_time = row[3]
             activity.plan_ahead = row[4]
+            activity.destination_id = row[5]
         else:
-            activity = cls(row[1], row[2], row[3], row[4])
+            # not in dictionary, create new instance and add to dictionary
+            activity = cls(row[1], row[2], row[3])
             activity.id = row[0]
             cls.all[activity.id] = activity
         return activity
@@ -184,30 +193,6 @@ class Activity:
             SELECT *
             FROM activities
             WHERE name is ?
-        """
-
-        row = CURSOR.execute(sql, (name,)).fetchone()
-        return cls.instance_from_db(row) if row else None
-
-    @classmethod
-    def find_by_price(cls, price):
-        """Return Activity object corresponding to first table row matching specified price"""
-        sql = """
-            SELECT *
-            FROM activities
-            WHERE price is ?
-        """
-
-        row = CURSOR.execute(sql, (name,)).fetchone()
-        return cls.instance_from_db(row) if row else None
-
-    @classmethod
-    def find_by_length_of_time(cls, length_of_time):
-        """Return Activity object corresponding to first table row matching specified length_of_time"""
-        sql = """
-            SELECT *
-            FROM activities
-            WHERE length_of_time is ?
         """
 
         row = CURSOR.execute(sql, (name,)).fetchone()
